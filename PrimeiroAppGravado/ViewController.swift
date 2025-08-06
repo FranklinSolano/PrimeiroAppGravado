06487122a3cbfaddaec9b195f81621fbdf26c644
FranklinSolano <100084667+FranklinSolano@users.noreply.github.com>
2025-08-06 17:32:36 -0300

    Initial Commit

---
+++ PrimeiroAppGravado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppGravado
+//
+//  Created by Franklin  Stilhano Solano on 06/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
